e979a84c4857deac9ca8f8b8d70250893f564c81
Bharath-serman <bharathserman@gmail.com>
2026-06-02 20:40:02 +0530

    Added AudioFade Logic and also added Audios for Main Menu Theme and Button click

--- Assets/Scripts/MainMenuLogic.cs
+++ Assets/Scripts/MainMenuLogic.cs
@@ -8,6 +8,8 @@ public class MainMenuLogic : MonoBehaviour
     [Header("Inputs")]
     [SerializeField] private Animator Fade_BG;
     [SerializeField] private Animator Load_Fade;
+    [SerializeField] private AudioSource Button_Click_Sound;
+    [SerializeField] private AudioSource MainMenuTheme;
 
     private string[] TriggerNames = {
         "Fade",
@@ -17,6 +19,8 @@ public class MainMenuLogic : MonoBehaviour
     string SceneName = "Game_Scene";
 
     float DelayTime = 1.5f;
+    float AudioFadeDuration = 1.5f;
+
     bool Status = false;
     [SerializeField] private GameObject LoadText;
     [SerializeField] private GameObject MainPanel;
@@ -27,6 +31,9 @@ public class MainMenuLogic : MonoBehaviour
     #region StartGameLogic
     IEnumerator StartGame()
     {
+        //Play the Button Click Sound.
+        Button_Click_Sound.Play();
+
         //Trigger the Fade Animation.
         Fade_BG.SetTrigger(TriggerNames[0]);
 
@@ -61,6 +68,7 @@ public class MainMenuLogic : MonoBehaviour
 
     public void BeginGame()
     {
+        StartCoroutine(FadeAudioVolume());
         StartCoroutine(StartGame());
     }
 
@@ -85,6 +93,24 @@ public class MainMenuLogic : MonoBehaviour
     {
         //Quit the Application.
         Application.Quit();
+    }
+    #endregion
+
+    #region AudioFadeLogic
+    IEnumerator FadeAudioVolume()
+    {
+        //Get the Actual Volume of the Audio Source.
+        float ActualVolume = MainMenuTheme.volume;
+
+        while (ActualVolume > 0f)
+        {
+            MainMenuTheme.volume -= Time.deltaTime / AudioFadeDuration;
+            yield return null;
+        }
+
+        MainMenuTheme.Stop();
+        MainMenuTheme.volume = ActualVolume;
+
     } 
     #endregion
 
